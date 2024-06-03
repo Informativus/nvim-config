ec654c63df7c884e6e925d1d2565a59a5dfc78da
Informativus <ivanpopov123321123@gmail.com>
2024-06-03 19:25:10 +0300

    update plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -27,5 +27,18 @@ require("lazy").setup({
         'akinsho/bufferline.nvim',
         version = "*",
         dependencies = 'nvim-tree/nvim-web-devicons'
-    }, {'lewis6991/gitsigns.nvim'}, {'terrortylor/nvim-comment'}
+    }, {'lewis6991/gitsigns.nvim'}, {'terrortylor/nvim-comment'},
+    {'akinsho/toggleterm.nvim', version = "*", config = true}, {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    }
 })
