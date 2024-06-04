3e3a9ef18c33c6227feb0bc0937cae41ce76b6a6
Informativus <ivanpopov123321123@gmail.com>
2024-06-04 13:54:58 +0300

    update mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -20,12 +20,10 @@ vim.keymap.set('n', '<leader>w', ':w<CR>')
 vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
 vim.keymap.set('n', '<leader>X', ':BufferLineCloseRight<CR>')
 vim.keymap.set('n', '<leader>s', ':BufferLineSortByTabs<CR>')
-vim.keymap.set('n', '<leader>tab', ':BufferLineCycleNext<CR>')
-vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('n', '<leader>i', ':BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
 -- Tabs
-vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
 
 -- Terminal
